1fbbdd203b9993ae121cab5cedd5e4cb83d621d8
litingfeng <420191163@qq.com>
2018-05-11 12:25:49 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo --theme=ananke --baseUrl="https://litingfeng.github.io/
+
+# Go To Public folder
+cd public
+cp -r . ../../litingfeng.github.io/
+cd ../../litingfeng.github.io/
+
+# Add changes to git.
+git init
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push https://github.com/litingfeng/litingfeng.github.io.git master
+
+# Come Back up to the Project Root
+cd ..
